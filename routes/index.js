b2ce5e5458875393d40f7c8f93643581d080b272
snogcel <ledniknoj@gmail.com>
2016-02-01 21:01:44 -0700

    Add DAPI Command - get_profile

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get('/api/dapi_command/addr/from_uid/:from_uid/to_uid/:to_uid/signature/:
   obj.data.command = "message";
   obj.data.subcommand = "addr";
   obj.data.from_uid = req.params.from_uid; // snogcel
-  obj.data.to_uid = req.params.to_uid; // snogcel
+  obj.data.to_uid = req.params.to_uid; // other user
   obj.data.signature = req.params.signature; // ??
   obj.data.payload = req.params.payload; // ENCRYPTED
 
@@ -34,6 +34,22 @@ router.get('/api/dapi_command/addr/from_uid/:from_uid/to_uid/:to_uid/signature/:
   });
 });
 
+// from electrum-dash evolution: {"object": "dapi_command", "data": {"slot": 1, "command": "get_profile", "id": "2627738", "to_uid": "snogcel", "from_uid": "snogcel"}}
+router.get('/api/dapi_command/get_profile/from_uid/:from_uid/to_uid/:to_uid/id/:id/slot/:slot', function(req, res, next) { // should we consider using POST? https://codeforgeek.com/2014/09/handle-get-post-request-express-4/
+  var obj = new Object;
+  obj.object = "dapi_command";
+  obj.data = new Object;
+  obj.data.command = "get_profile";
+  obj.data.from_uid = req.params.from_uid; // snogcel
+  obj.data.to_uid = req.params.to_uid; // snogcel
+  obj.data.id = req.params.id; // 2627738 (according to python this is random generated)
+  obj.data.slot = req.params.slot; // 1
+
+  client.dapi(JSON.stringify(obj), function(err, info, resHeaders) {
+    if (err) return console.log(err);
+    res.json(info); // server response
+  });
+}); // causes dashd to crash if called -- requires websocket connection (apparently)
 
 router.get('/api/getInfo', function(req, res, next) {
   client.getInfo(function(err, info, resHeaders) {
